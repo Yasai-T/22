3a3b3747abdda830bafd20c12912cc65578a163c
tomoki <sk.mk.ys.mt@gmail.com>
2021-12-29 00:41:37 +0900

    feat: toggle is complete

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,24 +1,55 @@
-import { Badge, Box, Heading, HStack, Stack, Text } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import {
+  Badge,
+  Box,
+  Checkbox,
+  Heading,
+  HStack,
+  IconButton,
+} from "@chakra-ui/react";
 import { VFC } from "react";
-import { Todo } from "../recoil/todo";
+import { useRecoilState } from "recoil";
+import { Todo, todoListState } from "../recoil/todo";
 
 type Props = {
   todo: Todo;
 };
 
+function replaceItemAtIndex<T>(arr: T[], index: number, newValue: T) {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+
+function removeItemAtIndex<T>(arr: T[], index: number) {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
 export const TodoCard: VFC<Props> = ({ todo }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((t) => t === todo);
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...todo,
+      isComplete: !todo.isComplete,
+    });
+    setTodoList(newList);
+  };
+
   return (
     <Box maxW="sm" borderWidth="thin" borderRadius="lg" p="6">
-      <HStack>
-        {todo.isComplete ? (
-          <Badge colorScheme={"green"} variant={"outline"}>
-            DONE
-          </Badge>
-        ) : (
-          <Badge colorScheme={"red"} variant={"outline"}>
-            TODO
-          </Badge>
-        )}
+      <HStack justifyContent="space-between">
+        <Checkbox isChecked={todo.isComplete} onChange={toggleItemCompletion}>
+          {todo.isComplete ? (
+            <Badge colorScheme={"green"} variant={"outline"}>
+              DONE
+            </Badge>
+          ) : (
+            <Badge colorScheme={"red"} variant={"outline"}>
+              TODO
+            </Badge>
+          )}
+        </Checkbox>
+        <IconButton aria-label="edit todo item" isRound icon={<EditIcon />} />
       </HStack>
       <Heading as="h4" size="lg" py="2">
         {todo.title}
